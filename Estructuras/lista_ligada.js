4ae5bc06174e9290daeb2d8d26a0384c62ff6f99
dinonino17 <danie.larios.t@gmail.com>
2018-08-13 18:36:38 -0500

    Actualizacion en funnciones para listas ligadas

--- Estructuras/lista_ligada.js
+++ Estructuras/lista_ligada.js
@@ -4,6 +4,7 @@ class Lista {
         //En este caso el ultimo nodo apunta a null asì sabes que ya terminó
         this.head = null;
     }
+ 
     addHead(valor){
         const newNode = {valor:valor};
         newNode.next = this.head;
@@ -11,6 +12,7 @@ class Lista {
         return this;
 
     }
+
     removeHead() {
         if (this.head === null) return false;
         this.head = this.head.next;
@@ -25,15 +27,39 @@ class Lista {
         }
         return undefined;
     }
+    removeTail(){
+            /*let tempNode = new Lista;
+            let tempNode2 = new Lista;
+            while(this.head){
+                tempNode.addHead(this.head.valor);
+                this.head = this.head.next;
+            }
+            tempNode.removeHead()
+            console.log(tempNode);
+            while(tempNode.head.next){
+                tempNode2.addHead(tempNode.head.valor);
+                tempNode.head = tempNode.head.next;
+            }
+            return tempNode*/
+            let tempNode = new Lista;
+            let listToReturn = new Lista;
+            while(this.head.next) {
+                listToReturn.addHead(this.head)
+                this.head = this.head.next;
+            }
+            return listToReturn;
+        }
+    
 }
 
 let nuevaLista = new Lista ();
 console.log(nuevaLista);
 nuevaLista.addHead(5);
 nuevaLista.addHead(4);
-nuevaLista.addHead("a");
-console.log(nuevaLista.find(4))
-nuevaLista.removeHead();
-console.log(nuevaLista);
+nuevaLista.addHead(6);
+console.log(nuevaLista)
+console.log(nuevaLista.removeTail())
+
+
 //quitar por valor
 //remover al final 
